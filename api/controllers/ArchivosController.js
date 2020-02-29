20519cce10f528920524dd6a3494f621bc8d90dc
jecontreras <joseeduar147@gmail.com>
2020-02-29 11:31:24 -0500

    cambios de files

--- api/controllers/ArchivosController.js
+++ api/controllers/ArchivosController.js
@@ -9,6 +9,15 @@ let Procedures = Object();
 Procedures.file = async(req, res)=>{
     let params = req.allParams();
     req.file('file').upload({
+      dirname: require('path').resolve(sails.config.appPath, 'assets/images')
+    },function (err, uploadFiles) {
+      if (err) return res.serverError(err);
+      sails.log.info(98, uploadFiles);
+      uploadFiles = uploadFiles[0].fd;
+      uploadFiles = (uploadFiles.split("images"))[1];
+      res.ok('images'+uploadFiles);
+    });
+    /*req.file('file').upload({
         //dirname: require('path').resolve(sails.config.appPath, 'assets/images')
         dirname: require('path').resolve(sails.config.appPath, '.tmp/public/images')
     },function (err, uploadFiles) {
@@ -16,10 +25,7 @@ Procedures.file = async(req, res)=>{
             return reject(err);
         }
         // sails.log.info(98, uploadFiles);
-        req.file('file').upload({
-            dirname: require('path').resolve(sails.config.appPath, 'assets/images')
-            //dirname: require('path').resolve(sails.config.appPath, '.tmp/public/images')
-        },function (err, uploadFiles) {
+        req.file('file').upload(function (err, uploadFiles) {
             if(err){
                 return reject(err);
             }
@@ -31,7 +37,7 @@ Procedures.file = async(req, res)=>{
         ;
         //res.ok(uploadFiles);
     })
-    ;
+    ;*/
 }
 
 module.exports = Procedures;
