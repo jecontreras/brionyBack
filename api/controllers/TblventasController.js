958e282b266db6155efa608ed191d141c83359be
jecontreras <joseeduar147@gmail.com>
2020-03-23 19:04:24 -0500

    cambios

--- api/controllers/TblventasController.js
+++ api/controllers/TblventasController.js
@@ -19,14 +19,14 @@ Procedures.querys = async (req, res)=>{
 	return res.ok(resultado);
 }
 
-Procedures.update = async ( req, res)=>{
-	let  params = req.allParams();
-	let resultado = Object();
-	params = _.omit(params, ['id', 'createdAt', 'updatedAt']);
-	params.usu_clave_int = await Tblusuario.findOne({ usu_email: params.ven_usu_creacion });
-	params.usu_clave_int = params.usu_clave_int.id;
-	resultado = await Tblventas.create( params );
-	return res.ok(resultado);
-}
+// Procedures.update = async ( req, res)=>{
+// 	let  params = req.allParams();
+// 	let resultado = Object();
+// 	params = _.omit(params, ['id', 'createdAt', 'updatedAt']);
+// 	params.usu_clave_int = await Tblusuario.findOne({ usu_email: params.ven_usu_creacion });
+// 	params.usu_clave_int = params.usu_clave_int.id;
+// 	resultado = await Tblventas.create( params );
+// 	return res.ok(resultado);
+// }
 
 module.exports = Procedures;
