8f65152bc0118418c8d236cd99cea61b569cd8c4
jecontreras <joseeduar147@gmail.com>
2020-04-07 18:20:05 -0500

    cambios de empresa

---
+++ api/models/Empresa.js
@@ -0,0 +1,55 @@
+/**
+ * Empresa.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    logo:{
+        type: 'string'
+    },
+    nombreTienda:{
+        type: 'string',
+        required: true
+    },
+    numeroCelular:{
+        type: 'string'    
+    },
+    emailTienda:{
+        type: 'string'      
+    },
+    direccionTienda:{
+        type: 'string'        
+    },
+    decripcion:{
+        type: 'string'          
+    },
+    foto1:{
+        type: 'json'
+    },
+    foto2:{
+        type: 'json'
+    },
+    portada1:{
+        type: 'string'
+    },
+    comentario1:{
+        type: 'string'
+    },
+    portada2:{
+        type: 'string'
+    },
+    portada3:{
+        type: 'string'
+    },
+    portada4:{
+        type: 'string'
+    }
+  },
+
+};
+
