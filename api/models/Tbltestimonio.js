ad44a36b306d68c516fb8cf4172bc5ab54f82634
jecontreras <joseeduar147@gmail.com>
2020-04-20 22:23:22 -0500

    cambios

---
+++ api/models/Tbltestimonio.js
@@ -0,0 +1,22 @@
+/**
+ * Tbltestimonio.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    usuario:{
+      model: 'tblusuario'
+    },
+    descripcion:{
+      type: 'string'
+    }
+
+  },
+
+};
+
