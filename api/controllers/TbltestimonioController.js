d3259ece3771796946be028124f41cd0e0f2c51d
jecontreras <joseeduar147@gmail.com>
2020-04-20 22:29:01 -0500

    cambios

--- api/controllers/TbltestimonioController.js
+++ api/controllers/TbltestimonioController.js
@@ -5,8 +5,17 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
-module.exports = {
-  
+let Procedures = Object();
+const _ = require('lodash');
 
-};
+Procedures.querys = async (req, res)=>{
+	let params = req.allParams();
+    let resultado = Object();
+	resultado = await QuerysServices(Tbltestimonio, params);
+	for(let row of resultado.data){
+		row.usuario = await Tblusuario.findOne({ id: row.usuario });
+	}
+	return res.ok(resultado);
+}
 
+module.exports = Procedures;
